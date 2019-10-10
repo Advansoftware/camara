a1bad69d9a3694c628b5ff53ca0684374edf7f45
bruno <brunoantunes94@hotmail.com>
2019-10-10 02:26:35 -0300

    correçoes de erros

--- application/models/Vereador_model.php
+++ application/models/Vereador_model.php
@@ -8,11 +8,15 @@
 
 		    public function get_vereador(){
 				$query = $this->db->query("Select v.*, p.imagem as imagem_partido from vereadores v inner join partidos p on v.partido = p.id
-			order  by nome asc");
+			  where v.ativo=1 order  by nome asc");
 		      return $query->result();
 		 
-		    }  
-
+		    }
+		    //Consulta que retorna quais vereadores estão ligados em cada pedido
+		public  function  get_dados_ligados_vereador_pedidosById($id){
+			$query = $this->query("SELECT * FROM vereador_pedido vp INNER JOIN vereadores v on v.id = vp.id_vereador WHERE vp.id_pedido = $id");
+			return $query->result_array();
+		}
 		public function get_providencia($Ids){
 			$query = $this->db->query("SELECT p.nome as nome_pedido, p.data_publicacao, year(p.data_publicacao) as ano, p.arquivo, v.nome as nome_vereador from pedidos p INNER JOIN vereador_pedido vp on p.id = vp.id_pedido INNER JOIN vereadores v on v.id = vp.id_vereador WHERE v.id =".$Ids." order by p.nome desc");
 			return $query->result_array();
